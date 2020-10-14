ae5044a6ea913ba2a5c6bf7b197edea996e9013f
AGKhalil <elghazalykhalil@gmail.com>
2020-10-14 11:30:08 +0000

    argument name changed to something more fitting

--- train.py
+++ train.py
@@ -26,9 +26,9 @@ def main(hparams) -> None:
     else:
         hparams.logger = WandbLogger(project=hparams.project)
     seed_everything(hparams.seed)
-    model = squiRL.reg_algorithms[hparams.model](hparams)
+    algorithm = squiRL.reg_algorithms[hparams.algorithm](hparams)
     trainer = pl.Trainer.from_argparse_args(hparams)
-    trainer.fit(model)
+    trainer.fit(algorithm)
 
 
 parser = argparse.ArgumentParser()
@@ -36,9 +36,9 @@ parser = argparse.ArgumentParser()
 # add PROGRAM level args
 parser.add_argument('--seed', type=int, default=42)
 parser.add_argument('--debug', type=bool, default=False)
-parser.add_argument('--model', type=str, default='VPG')
+parser.add_argument('--algorithm', type=str, default='VPG')
 args, _ = parser.parse_known_args()
-parser.add_argument('--project', type=str, default=args.model)
+parser.add_argument('--project', type=str, default=args.algorithm)
 
 # add environment specific args
 parser.add_argument("--env",
@@ -54,8 +54,8 @@ parser.add_argument("--max_episode_reward",
                     default=200,
                     help="max episode reward in the environment")
 
-# add model specific args
-parser = squiRL.reg_algorithms[args.model].add_model_specific_args(parser)
+# add algorithm specific args
+parser = squiRL.reg_algorithms[args.algorithm].add_model_specific_args(parser)
 
 # add all the available trainer options to argparse
 parser = pl.Trainer.add_argparse_args(parser)
