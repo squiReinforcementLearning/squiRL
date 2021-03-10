e9ca79182f5b8c6d34ac5d3ac718f8bfa1d457a3
AGKhalil <elghazalykhalil@gmail.com>
2021-03-11 00:26:13 +0200

    all args except for logger, gpus, tpu_cores are saved to a json file. args can now be loaded from json file. commandline args trump json args. added an example config file to vpg

--- train.py
+++ train.py
@@ -6,11 +6,14 @@ Attributes:
     args (argparse.Namespace): Parsed config arguments for running script
     parser (argparse.ArgumentParser): Argument parser
 """
+import os
+import json
 import argparse
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.utilities.seed import seed_everything
 import pytorch_lightning as pl
 import squiRL
+import gym
 from pytorch_lightning.profiler import AdvancedProfiler
 
 
@@ -20,12 +23,30 @@ def train(hparams) -> None:
     Args:
         hparams (argparse.Namespace): Stores all passed args
     """
-    if hparams.debug is True:
+    if hparams.debug:
         hparams.logger = None
         profiler = True
     else:
         hparams.logger = WandbLogger(project=hparams.project)
+        hparams.logger.experiment
         profiler = None
+
+    cwd = os.getcwd()
+    path = os.path.join(cwd, 'models')
+    if not os.path.exists(path):
+        os.mkdir(path)
+    path = os.path.join(path, hparams.logger.version)
+    if not os.path.exists(path):
+        os.mkdir(path)
+    path = os.path.join(path, hparams.logger.version)
+    if hparams.save_config:
+        with open(path + '.json', 'wt') as f:
+            config = vars(hparams).copy()
+            config.pop("logger")
+            config.pop("gpus")
+            config.pop("tpu_cores")
+            json.dump(config, f, indent=4)
+
     seed_everything(hparams.seed)
     algorithm = squiRL.reg_algorithms[hparams.algorithm](hparams)
     trainer = pl.Trainer.from_argparse_args(hparams, profiler=profiler)
@@ -39,6 +60,14 @@ if __name__ == '__main__':
     group_env = parser.add_argument_group("environment_args")
 
     # add PROGRAM level args
+    parser.add_argument(
+        '--save_config',
+        type=bool,
+        default=True,
+        help='Save settings to file in json format. Ignored in json file')
+    parser.add_argument('--load_config',
+                        type=str,
+                        help='Load from json file. Command line override.')
     group_prog.add_argument('--seed',
                             type=int,
                             default=42,
@@ -65,14 +94,21 @@ if __name__ == '__main__':
                            type=str,
                            default="CartPole-v0",
                            help="gym environment tag")
-    group_env.add_argument("--episode_length",
-                           type=int,
-                           default=200,
-                           help="max length of an episode")
-    group_env.add_argument("--max_episode_reward",
-                           type=int,
-                           default=200,
-                           help="max episode reward in the environment")
+    args, remaining_args = parser.parse_known_args()
+    env = gym.make(args.env)
+    parser.add_argument('--observation_space',
+                        type=int,
+                        default=env.observation_space.shape[0],
+                        help='env state space')
+    parser.add_argument('--action_space',
+                        type=int,
+                        default=env.action_space.n,
+                        help='env action space')
+    parser.add_argument('--max_reward',
+                        type=int,
+                        default=env.spec.reward_threshold,
+                        help='Max reward allowed')
+    env.close()
 
     # add algorithm specific args
     group_alg = squiRL.reg_algorithms[args.algorithm].add_model_specific_args(
@@ -87,7 +123,11 @@ if __name__ == '__main__':
         epilog="Trainer args docs can be found at PyTorch Lightning.")
 
     args = parser.parse_args()
-    args, _ = parser.parse_known_args()
+    if args.load_config:
+        with open(args.load_config, 'rt') as f:
+            t_args = argparse.Namespace()
+            t_args.__dict__.update(json.load(f))
+            args = parser.parse_args(namespace=t_args)
 
     print(args)
 
